3d6ec954091a2ef0c7ad12762596118cd9277bd3
FelicityBlue <nb29691@umbc.edu>
2024-01-09 10:12:58 -0500

    create TEXT class

--- aimTrainer.h
+++ aimTrainer.h
@@ -14,90 +14,70 @@ public:
     sf::RectangleShape button;
     // getters
     sf::Vector2f getSize() {
-        return currSize;
+        return button.getSize();
     }
     sf::Vector2f getPos() {
-        return currPos;
+        return button.getPosition();
     }
     sf::Color getColor() {
-        return currColor;
+        return button.getFillColor();
     }
     // setters
     void setNewSize(sf::Vector2f newSize) {
-        currSize = newSize;
         button.setSize(newSize);
     }
     void setNewPos(sf::Vector2f newPos) {
-        currPos = newPos;
         button.setPosition(newPos.x, newPos.y);
     }
     void setNewColor(sf::Color newColor) {
-        currColor = newColor;
         button.setFillColor(newColor);
     }
     // create
-    BUTTON(sf::Color color, sf::Vector2f size, sf::Vector2f pos) : currColor(color), currPos(pos), currSize(size) {
+    BUTTON(sf::Color color, sf::Vector2f size, sf::Vector2f pos) {
         button.setFillColor(color);
         button.setPosition(pos.x, pos.y);
         button.setSize(size);
-    };
-private:
-    sf::Vector2f currSize;
-    sf::Vector2f currPos;
-    sf::Color currColor;
-
+    }
 };
+
+
 
 class TEXT {
 public:
     sf::Text text;
+
     // getters
     sf::Font getFont() {
-        return currFont;
+        return *text.getFont();
     }
     sf::Vector2f getPos() {
-        return currPos;
+        return text.getPosition();
     }
     sf::Color getColor() {
-        return currColor;
+        return text.getFillColor();
     }
     std::string getString() {
-        return currString;
+        return text.getString();
     }
     // setters
     void setNewFont(sf::Font newFont) {
-        currFont = newFont;
         text.setFont(newFont);
     }
     void setNewPos(sf::Vector2f newPos) {
-        currPos = newPos;
         text.setPosition(newPos.x, newPos.y);
     }
     void setNewColor(sf::Color newColor) {
-        currColor = newColor;
         text.setFillColor(newColor);
     }
     void setNewString(std::string newString) {
-        currString = newString;
         text.setString(newString);
     }
     // create
-    TEXT(sf::Color color, sf::Font font, sf::Vector2f pos, std::string string) : currColor(color), currPos(pos), currString(string), currFont(font) {
+    TEXT(sf::Color color, sf::Font& font, sf::Vector2f pos, std::string string) {
+
         text.setFillColor(color);
         text.setFont(font);
         text.setPosition(pos.x, pos.y);
         text.setString(string);
-    };
-    TEXT(sf::Font font, sf::Vector2f pos, std::string string) : currPos(pos), currString(string), currFont(font) {
-        text.setFont(font);
-        text.setPosition(pos.x, pos.y);
-        text.setString(string);
-    };
-private:
-    sf::Font currFont;
-    sf::Vector2f currPos;
-    std::string currString;
-    sf::Color currColor = sf::Color::White;
-
+    }
 };
-
